658c09b22ae6f8104083e374e8055346748e4778
JuneShi <juneshi0315@qq.com>
2016-10-08 22:51:34 +0800

    add a test file

--- src/S1000.java
+++ src/S1000.java
@@ -3,7 +3,7 @@ import java.io.*;
 /**
  * Created by junes on 2016/10/8.
  */
-public class Test {
+public class S1000 {
     public static void main(String[] args) throws IOException {
         StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
         PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
